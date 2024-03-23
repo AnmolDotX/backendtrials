8cc34729fda7b65ad9f6f1e10d1eda4e7a31a809
Anmol8611 <kumaranmol8611@gmail.com>
2024-03-23 11:55:07 +0530

    added register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -16,5 +16,10 @@ app.use(cookieParser());
 
 
 
+// Routes import
+import userRouter from './routes/user.routes.js'
+
+// Routes declaration
+app.use("/api/v1/users", userRouter)
 
 export {app}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 })
 
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id : this._id,
-            email : this._id,
+            email : this.email,
             username : this.username,
             fullname : this.fullname
         },
